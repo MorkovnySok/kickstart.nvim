ba4a4b09c83bdd8e4a776787a5b8ccf5921c5f70
Artem Vasilyev <artem.vasilev@sberins.ru>
2025-04-16 15:32:16 +0300

    some more keymaps

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -2,18 +2,23 @@ local map = vim.keymap.set
 
 map('n', '<leader>ll', ':%!dos2unix<CR>', { desc = 'Convert CRLF to LF' })
 
-map('n', '<C-t>', ':e %:p:r.ts<CR>', { desc = 'Go to typescript' })
-map('n', '<C-g>', ':e %:p:r.html<CR>', { desc = 'Go to html' })
-map('n', '<C-b>', ':e %:p:r.css<CR>', { desc = 'Go to css' })
+map('n', '<leader>fd', function()
+  local path = vim.fn.expand '%:p:h:t'
+  vim.fn.setreg('+', path)
+  print('Copied to clipboard: ' .. path)
+end, { desc = 'Copy [F]ile [D]irectory to clipboard' })
+-- map('n', '<C-t>', ':e %:p:r.ts<CR>', { desc = 'Go to typescript' })
+-- map('n', '<C-g>', ':e %:p:r.html<CR>', { desc = 'Go to html' })
+-- map('n', '<C-b>', ':e %:p:r.css<CR>', { desc = 'Go to css' })
 
 map('n', '<C-d>', '<C-d>zz')
 map('n', '<C-u>', '<C-u>zz')
 map('n', 'n', 'nzzzv')
 map('n', 'N', 'Nzzzv')
+map('n', '<leader>ih', '<C-w>|<C-w>_', { desc = 'Maximize buffer' })
 
-map('x', '<leader>p', [["_dP]])
+map('x', '<leader>p', '"_dP')
 map('i', '<C-c>', '<Esc>')
-
 map('n', '<C-Left>', "<cmd>lua require'utils'.resize(true, -5)<CR>")
 map('n', '<C-Right>', "<cmd>lua require'utils'.resize(true, 5)<CR>")
 map('n', '<C-Up>', "<cmd>lua require'./utils'.resize(false, -5)<CR>")
@@ -37,6 +42,15 @@ map('n', '<leader>ew', ':NvimTreeToggle<CR>', { desc = 'Open tree' })
 map('n', '<leader>ef', ':NvimTreeFindFile<CR>', { desc = 'Find file in a tree' })
 map('n', '<leader>ex', ':silent !explorer "%:p:h"<CR>', { desc = 'Find file in a tree', silent = true })
 
--- telescope
+-- open in code
+map('n', '<leader>fo', ':!code %', { desc = '[F]ile [O]pen in VsCode' })
+
+-- Enable Ctrl+hjkl movements in Insert mode
+map('i', '<C-h>', '<Left>', { desc = 'Move left in insert mode' })
+map('i', '<C-j>', '<Down>', { desc = 'Move down in insert mode' })
+map('i', '<C-k>', '<Up>', { desc = 'Move up in insert mode' })
+map('i', '<C-l>', '<Right>', { desc = 'Move right in insert mode' })
+
+map('n', '<leader>lR', ':LspRestart<CR>', { desc = 'Restart LSP' })
 
 return {}
