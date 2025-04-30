6602239ae8ec436988bd199e07839158a348ffdb
Artem Vasilyev <artem.vasilev@sberins.ru>
2025-04-30 18:51:41 +0300

    added langmap

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,14 @@
 --
 -- See the kickstart.nvim README for more information
 vim.deprecate = function() end
+vim.opt.fixeol = false
+
+vim.opt.langmap = table.concat({
+  'йq,цw,уe,кr,еt,нy,гu,шi,щo,зp,х[,ъ],фa,ыs,вd,аf,пg,',
+  "рh,оj,лk,дl,ж\\;,э',яz,чx,сc,мv,иb,тn,ьm,б\\,,ю.,",
+  'ЙQ,ЦW,УE,КR,ЕT,НY,ГU,ШI,ЩO,ЗP,Х{,Ъ},ФA,ЫS,ВD,АF,ПG,',
+  'РH,ОJ,ЛK,ДL,Ж:,Э",ЯZ,ЧX,СC,МV,ИB,ТN,ЬM,Б<,Ю>',
+}, '')
 
 return {
   'cohama/lexima.vim',
