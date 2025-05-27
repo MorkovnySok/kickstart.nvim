8b268ecb4dbafb62bc665a78181d518b1e3570f1
Artem Vasilyev <artem.vasilev@sberins.ru>
2025-05-27 23:37:43 +0300

    smart find files and live grep to use with nvim tree to search in folder if its selected in the tree

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -59,18 +59,49 @@ return { -- Fuzzy Finder (files, lsp, etc)
       },
     }
 
+    local builtin = require 'telescope.builtin'
+    local function get_target_path()
+      local api = require 'nvim-tree.api'
+      local node = api.tree.get_node_under_cursor()
+      if not node then
+        return nil
+      end
+
+      local is_folder = node.fs_stat and node.fs_stat.type == 'directory'
+      return is_folder and node.absolute_path or vim.fn.fnamemodify(node.absolute_path, ':h')
+    end
+
+    local function smart_search(opts)
+      local is_nvim_tree = vim.bo.filetype == 'NvimTree'
+      local search_fn = opts.use_live_grep and builtin.live_grep or builtin.find_files
+
+      local search_opts = vim.tbl_extend('force', opts, {
+        cwd = is_nvim_tree and get_target_path() or nil,
+      })
+
+      search_fn(search_opts)
+    end
+
+    -- Convenience wrappers
+    local function smart_find_files(opts)
+      smart_search(vim.tbl_extend('force', opts or {}, { use_live_grep = false }))
+    end
+
+    local function smart_live_grep(opts)
+      smart_search(vim.tbl_extend('force', opts or {}, { use_live_grep = true }))
+    end
+
     -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
 
     -- See `:help telescope.builtin`
-    local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', smart_find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sg', smart_live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
